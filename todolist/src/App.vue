0ad2b12b15e05401be3aafe69ea9004bc2738c3f
Wing <524905824@qq.com>
2021-06-29 20:21:13 +0800

    安装naive-ui，及初始化
    安装naive-ui内容，并且将主题调整为深色主题

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <n-config-provider :theme="darkTheme" :locale="zhCN" :date-locale="dateZhCN">
+		<n-space vertical>
+			<n-button>naive-ui</n-button>
+			<n-button type="info">测试</n-button>
+		</n-space>
+	</n-config-provider>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
+<script>
+import { defineComponent } from 'vue'
+import { NConfigProvider, NSpace } from 'naive-ui'
+import { darkTheme } from 'naive-ui'
+import { zhCN, dateZhCN } from 'naive-ui'
+import { NButton } from 'naive-ui'
+export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	components: {
+		NConfigProvider,
+		NButton,
+		NSpace
+	},
+	setup() {
+		return {
+			darkTheme,
+			zhCN,
+			dateZhCN
+		}
+	}
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	background: black;
 }
 </style>
